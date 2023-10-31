c110a6d6a6b960ac0c27037cf8c2b04906b0234f
Lakshmi Chandra <CHANDL02@heiway.net>
2023-10-31 11:39:23 +0530

    removed spaces

--- CrawlWebApp/Program.cs
+++ CrawlWebApp/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddHttpClient();
 builder.Services.AddResponseCaching();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
